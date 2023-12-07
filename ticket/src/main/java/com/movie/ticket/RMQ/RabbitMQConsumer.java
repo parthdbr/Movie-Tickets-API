8d2b6ff955b8ee84f080bef50e8062e33a8fe83f
Parth Dobariya <parthdbr@gmail.com>
2023-12-07 15:20:27 +0530

    Email Service applied

---
+++ ticket/src/main/java/com/movie/ticket/RMQ/RabbitMQConsumer.java
@@ -0,0 +1,14 @@
+package com.movie.ticket.RMQ;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RabbitMQConsumer {
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message){
+        log.info(String.format("Received message -> %s", message));
+    }
+}
