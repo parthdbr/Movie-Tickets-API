30ef893ec0fb005c44b67f57a8a07ae07576335f
Parth Dobariya <parthdbr@gmail.com>
2023-12-19 17:08:22 +0530

    TimeZone Added

--- ticket/src/main/java/com/movie/ticket/TicketApplication.java
+++ ticket/src/main/java/com/movie/ticket/TicketApplication.java
@@ -1,13 +1,23 @@
 package com.movie.ticket;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TicketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+//		System.out.println(new Date());
 	}
 
 }
